e9199b754b620c8de5abd115e6360524553c6adf
Q.R. Lai <tornado900113@hotmail.com>
2023-03-06 14:47:40 +0800

    initial revision

---
+++ src/main/java/org/practise/apache/beam/Main.java
@@ -0,0 +1,12 @@
+package org.practise.apache.beam;
+
+/*
+ * Copyright (c) 2023 <Q.R.> Lai.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
